a66f351f34d4994b9d6d09003a63e7a94625a3fa
udayhacks <71202015+udayhacks@users.noreply.github.com>
2022-01-04 11:24:54 +0530

    Update stack _using_linkedlist.c

--- stack _using_linkedlist.c
+++ stack _using_linkedlist.c
@@ -25,4 +25,79 @@ void main ()
     printf("\n1.Push \n 2.Pop \n 3.Shopw \n 4. Exit");
   
   
+  print("\n Enter your choice \n");
+        scanf("%d", &choice);
+        
+        
+        switch(choice)
+        {
+              case 1 : 
+            {
+               push();
+                  break ;
+            } 
+                    
+              case 2 :
+                    {
+                          pop (); 
+                          break;
+                    }
+                    
+                    
+              case 3 : 
+                    {
+                       display();
+                     break ;
+                    }
+                       
+                    
+              case 4 : 
+                    {
+                    
+                    printf("Exiting ...");
+                    }
+                    
+                    
+               default : 
+                    {
+                          printf("Please Enter valid choice ");
+                        
+                    }
+        };
   }
+}
+
+void push (0
+           { 
+                 int val ;
+                 struct node *ptr = (structnode *)malloc (sizeof(structnode ));
+                 if (ptr == NULL)
+                 { 
+                       printf(" not able to push the element");
+                 } 
+                  else { 
+                        printf("enter the  value ");
+                        scanf("%d ", &val);
+                        if (top == NULL) 
+                        { ptr ->= val ;
+                         ptr -> next = NULL ;
+                         top = ptr ;
+                        } 
+                        
+                        printf("Item pushed ");
+                  }
+           }
+           
+           void pop()
+           {  
+                 int item ; 
+                 struct node *ptr ;
+                 if (top == NULL )
+                 { 
+                       printf("Underflow ");
+                       
+                 }
+                 
+                 else 
+                 {
+                       i
